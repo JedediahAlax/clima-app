62732869831475c182e69d2041a4f62b5b149874
JedediahAlax <jedediah@me.com>
2021-09-27 22:26:35 -0600

    first commit. added ssh key to github and cloned project start.

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,19 @@
+// Clima weather app created as part of an online iOS boot camp.
+
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
